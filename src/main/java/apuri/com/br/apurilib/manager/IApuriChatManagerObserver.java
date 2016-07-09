6ecdfd4b67e377f1105a017a2f9fca6b3dedcdda
paulo.junior <apuridev@gmail.com>
2016-07-09 11:12:24 -0300

    not master for now

--- src/main/java/apuri/com/br/apurilib/manager/IApuriChatManagerObserver.java
+++
@@ -1,7 +0,0 @@
-package apuri.com.br.apurilib.manager;
-
-/**
- * Created by paulo.junior on 09/07/2016.
- */
-public interface IApuriChatManagerObserver {
-}
--- src/main/java/apuri/com/br/apurilib/model/IApuriUserData.java
+++
@@ -1,7 +0,0 @@
-package apuri.com.br.apurilib.model;
-
-/**
- * Created by paulo.junior on 01/07/2016.
- */
-public interface IApuriUserData {
-}
